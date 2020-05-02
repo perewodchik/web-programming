a0caac2581c4de004c440a20dbe2452d6dbba0c9
perewodchik <pickachu507@gmail.com>
2020-05-02 17:08:32 +0300

    Lesson 3 homework

---
+++ homework.js
@@ -0,0 +1,89 @@
+/**
+ * ДЗ-03 
+ */
+
+/**
+ * 0. Исправь ошибки
+ */
+function initVal(str) {
+    String s = "String";
+    int a = 10;
+    bool b = true;
+    const c = const;
+}
+
+/**
+ * 1. Напиши функцию которая принимает на вход строку и преобразует ее в строку
+ * @param {string} str 
+ * 
+ * @returns (number)
+ */
+function stringToNumber(str) {
+
+}
+
+/**
+ * 2. Функции возвращают значение val, если оно существует, иначе def
+ * @param {*} val
+ * @param {*} def
+ * 
+ * @returns val или def
+ */
+function checkVal1 (val, def) {
+    // 2.1. if ... else
+
+}
+
+function checkVal2 (val, def) {
+    // 2.2. тернарный оператор
+
+}
+
+function checkVal3 (val, def) {
+    // 2.3. логическое или
+
+}
+
+/**
+ * 3. Фунция рендера
+ * @param {string} title
+ * @param {number} width
+ * @param {number} height
+ * @param {bool} isBox
+ * 
+ * @returns {string} строка формата 'Товар title, шириной width, высотой height, коробка' или '... не коробка'
+ */
+function renderItem (title, width, height, isBox) {
+ 
+}
+
+/**
+ * 4. Напиши функцию oddNum с помощью цикла for
+ * @param {number} max максимальное число
+ * 
+ * @returns {string} только не четные 1 3 5 7 9 ...max 
+ */
+function oddNum (max) {
+    
+}
+
+/**
+ * 5. Напиши функцию factorial рекурсивно
+ * @param {number} n максимальное число для вычисления
+ * 
+ * @returns {number} факториал 
+ */
+function factorial(n) {
+    
+}
+
+module.exports = {
+    initVal,
+    stringToNumber,
+    renderItem,
+    checkVal1,
+    checkVal2,
+    checkVal3,
+    oddNum,
+    factorial
+};
